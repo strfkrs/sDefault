a0366e5110061cb71b0f8b1a47c64ccc0e37d170
= <base@mkoe.net>
2019-01-01 22:45:16 +0100

    room.creeps does not search for creeps with corresponding home anymore, instead finds actually present creeps

--- _Room.creeps.js
+++ _Room.creeps.js
@@ -3,7 +3,7 @@ Object.defineProperty(Room.prototype, 'creeps', {
         if (this._creeps == undefined) {
             
             if (this.memory.creepNames == undefined) {
-                this.memory.creepNames = this.find(FIND_MY_CREEPS, (c) => c.home == this).map(f => f.name);
+                this.memory.creepNames = this.find(FIND_MY_CREEPS).map(f => f.name);
             }
             this._creeps = this.memory.creepNames.map(name => Game.creeps[name]);
         }
