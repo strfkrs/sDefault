44ba8748c329353e20a3f402dfcf66d58fb9bf1b
= <base@mkoe.net>
2019-01-02 00:09:32 +0100

    cleanup

--- _Spawn.autoSpawn.js
+++ _Spawn.autoSpawn.js
@@ -1,18 +1,40 @@
 const l = require("logger");
 const utils = require("utils");
 
-var possibleRole = function(role, room) {
+var isPossibleRole = function(role, room) {
+    let ret = false;
+
     switch (role) {
-        default: return room.energyAvailable >= getSpawnData(role).minEnergy;
+        default: ret = room.energyAvailable >= getSpawnData(role).minEnergy;
     }
+
+
+    l.debug("Spawn.autoSpawn","role "+ROLES[role].name+" isPossible: "+ret);
+
+    return ret;
 }
 
-var necessaryRole= function(role, room) {
+var isNecessaryRole = function(role, room) {
+
+    let ret = false;
+
     switch (role) {
         case ROLE_WORKER:
-            return _.filter(room.creeps, (creep) => creep.role == ROLE_WORKER).length < 6 * room.sources.length;
-        default: l.warn("Spawn.autospawn: necessaryRole switch default reached");
+        
+        ret = true;
     }
+
+    l.debug("Spawn.autoSpawn","role "+ROLES[role].name+" isNecessary: "+ret);
+
+    return ret;
+}
+
+var getPossibleRoles = function(room) {
+    let possibleRoles = _.filter(
+        [ ROLE_WORKER ],
+        (role) => isNecessaryRole(role, room) && isPossibleRole(role, room));
+    
+    return possibleRoles;
 }
 
 var getSpawnData = function(role) {
@@ -27,22 +49,17 @@ var getRoleData = function (role) {
 
 Spawn.prototype.autoSpawn = function() {
     if (!this.spawning) {
-        let msg = "Spawn.autoSpawn";
         let actionMsg = "";
-        const room = this.room;
-        const roles = [ ROLE_WORKER ];
-        msg += " allroles: "+roles.length;
 
-        const necessaryRoles = _.filter(roles, (role) => necessaryRole(role, room));
-        msg += " necessaryRoles: "+necessaryRoles.length;
-        
-        if (necessaryRoles.length) {
-            const possibleRoles = _.filter(necessaryRoles, (role) => necessaryRole(role, room) && possibleRole(role, room));
+        const room = this.room;
+        const possibleRoles = getPossibleRoles(room);
+
+        if (possibleRoles.length) {
     
-            msg += " possibleRoles: "+possibleRoles.length;
+            let msg = "autoSpawn: possible roles: "+possibleRoles.length;
             if (possibleRoles.length) {
+                let msg2 = "";
     
-                msg += " - neededLen: "+possibleRoles.length;
                 const role = possibleRoles[0];
                 const name = utils.createCreepName(ROLES[role].name);
                 const data = getRoleData(role);
@@ -51,7 +68,7 @@ Spawn.prototype.autoSpawn = function() {
                 const energy = (room.energyAvailable > getSpawnData(role).maxEnergy) ? getSpawnData(role).maxEnergy : room.energyAvailable;
                 const bodyParts = getSpawnData(role).bodyParts;
 
-                msg += " - creating body: energy: "+energy+" bodyParts: "+bodyParts;
+                msg2 += " - creating body: energy: "+energy+" bodyParts: "+bodyParts;
 
                 const body = utils.createCreepBody(
                     energy,
@@ -64,17 +81,18 @@ Spawn.prototype.autoSpawn = function() {
                 if ( status == OK )
                 {
                     const creep = Game.creeps[name];
-                    msg += " -> spawned - setting home: "+creep.home.name;
+                    msg2 += " -> spawned - setting home: "+creep.home.name;
                     actionMsg = "-->";
                     creep.room.creepsAdd(creep);
                     
                 } else {
-                    msg += " -> status "+status;
+                    msg2 += " -> status "+status;
 
-                    msg += " (args: "+body+", "+name+",  "+data+")"+status;
+                    msg2 += " (args: "+body+", "+name+",  "+data+")"+status;
                 }
+                l.og("",this,actionMsg,msg2);
             }
+            l.og("",this,actionMsg,msg);
         }
-        l.og("",this,actionMsg,msg);
     }
 }
