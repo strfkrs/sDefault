d9d34ad4304a2f98107fa1326d11fb63a51823e6
= <base@mkoe.net>
2019-01-01 06:09:09 +0100

    test

--- _Creep.run.js
+++ _Creep.run.js
@@ -6,56 +6,57 @@ const JOBPADDING = "                              ";
 
 Creep.prototype.run = function () {
 
-    let gotAssignedActions = false;
+    if (!this.spawning) {
 
-
-    /*
-     *      get jobs from role
-     */
-    if (this.job == JOB_UNDEFINED) {
-        this.job = ROLES[this.role].getJob(this);
-    }
-
-    /*
-     *      get actions from job
-     */
-    if (!this.actions.length) {
-        this.actions = JOBS[this.job].getActions(this);
-        gotAssignedActions = true;
-    }
-
-
-
-    /*
-     *      run action
-     */
-    let actions = this.actions;
-
-    //let actionMsg = this.actions.map(a => ACTIONS[a].name.padding(ACTIONPADDING,false)).join(", ");
-    let actionMsg = this.actions.map(a => ACTIONS[a].name).join(", ")
-
-    l.og("",this, ((gotAssignedActions) ? " assigned > " : ""),
-        "JOB: "+JOBS[this.job].name.padding(JOBPADDING,false) +
-        " ACTIONS: "+actionMsg );
-
-
-    while (actions.length) {
+        let gotAssignedActions = false;
     
-        if (ACTIONS[actions[0]].run(this)) {
-            l.debug("Creep.run","action "+actions[0]+"returned true");
-            break;
+        /*
+         *      get jobs from role
+         */
+        if (this.job == JOB_UNDEFINED) {
+            this.job = ROLES[this.role].getJob(this);
         }
-        else {
-            l.debug("Creep.run","action "+actions[0]+"returned false");
-            actions.shift();
-            this.actions = actions;
+    
+        /*
+         *      get actions from job
+         */
+        if (!this.actions.length) {
+            this.actions = JOBS[this.job].getActions(this);
+            gotAssignedActions = true;
+        }
+    
+    
+    
+        /*
+         *      run action
+         */
+        let actions = this.actions;
+    
+        //let actionMsg = this.actions.map(a => ACTIONS[a].name.padding(ACTIONPADDING,false)).join(", ");
+        let actionMsg = this.actions.map(a => ACTIONS[a].name).join(", ")
+    
+        l.og("",this, ((gotAssignedActions) ? " assigned > " : ""),
+            "JOB: "+JOBS[this.job].name.padding(JOBPADDING,false) +
+            " ACTIONS: "+actionMsg );
+    
+    
+        while (actions.length) {
+        
+            if (ACTIONS[actions[0]].run(this)) {
+                l.debug("Creep.run","action "+actions[0]+" -> returned true");
+                break;
+            }
+            else {
+                l.debug("Creep.run","action "+actions[0]+" -> returned false");
+                actions.shift();
+                this.actions = actions;
+                this.focus = undefined;
+            }
+        }
+    
+        if (!actions.length) {
+            this.job = JOB_UNDEFINED;
             this.focus = undefined;
         }
     }
-
-    if (!actions.length) {
-        this.job = JOB_UNDEFINED;
-        this.focus = undefined;
-    }
-
 }
