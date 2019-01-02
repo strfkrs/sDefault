27cc94657074f690b202451b7e0fe099c0d274c9
= <base@mkoe.net>
2019-01-02 04:39:45 +0100

    added const action for actions[0]

--- _Creep.runActions.js
+++ _Creep.runActions.js
@@ -10,18 +10,21 @@ Creep.prototype.runActions = function () {
     
     while (actions.length) {
 
+
         l.og("",this,"",
             "JOB: "+JOBS[this.job].name.padding(JOBPADDING,false) +
             " ACTIONS: "+this.actions.map(a => ACTIONS[a].name).join(", ")
         );
+        
+        const action = ACTIONS[actions[0]];
 
-        if (ACTIONS[actions[0]].run(this)) {
-            l.debug("Creep.runActions","action "+actions[0]+" -> returned true");
+        if (action.run(this)) {
+            l.debug("Creep.runActions","action "+action.name+" -> returned true");
             return true;
 
         }
         else {
-            l.debug("Creep.runActions","action "+actions[0]+" -> returned false");
+            l.debug("Creep.runActions","action "+action.name+" -> returned false");
             actions.shift();
             this.actions = actions;
             this.focus = undefined;
